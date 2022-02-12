f3e05d4f727898964be83be1b2d80490141c32a5
George Sofianos <georgesofianosgr@gmail.com>
2022-02-12 18:26:55 +0200

    chore: add stylua sto null ls

--- lua/plugin_config/null_ls_config.lua
+++ lua/plugin_config/null_ls_config.lua
@@ -7,7 +7,8 @@ module.setup = function()
     sources = {
       -- null_ls.builtins.diagnostics.eslint,
       -- null_ls.builtins.code_actions.eslint,
-      null_ls.builtins.formatting.prettier
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua
     },
     on_attach = function(client)
         if client.resolved_capabilities.document_formatting then
